4683f9460a21036e0d828b0576bf18aa0a387a16
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-10-24 09:59:54 +0900

    detailで完結するようになったのでvoteのurlを削除

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
   url(r'^$', views.IndexView.as_view(), name = 'index'),
   url(r'^(?P<pk>\d+)/$', views.detail, name = 'detail'),
-  url(r'(?P<pk>\d+)/vote$', views.vote, name = 'vote'),
   url(r'(?P<pk>\d+)/results$', views.ResultsView.as_view(), name = 'results'),
   url(r'^form$', views.form_test, name = 'form'),
 ]
