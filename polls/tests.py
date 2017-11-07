dce55e5d244dd6da7e507304c508b187026987cb
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-11-07 11:38:56 +0900

    indexの表示テスト追加

--- polls/tests.py
+++ polls/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.utils import timezone
 from datetime import timedelta
+from django.shortcuts import resolve_url
+from unittest import mock
 from .models import Question
 
 # Create your tests here.
@@ -17,3 +19,18 @@ class PollTest(TestCase):
 
         obj = Question(published_date = timezone.now() + timedelta(minutes = 1))
         self.assertFalse(obj.was_published_recently(), '1分後に公開')
+
+class ViewTest(TestCase):
+    def test_index(self):
+        response = self.client.get(resolve_url('polls:index')) # 指定URLへアクセス
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(0, response.context['questions'].count()) # テスト時のDBは毎回初期状態から始まる
+
+        # 新規データを作成
+        Question.objects.create(
+            question_text = "aaa",
+            published_date = timezone.now(),
+        )
+        response = self.client.get(resolve_url('polls:index')) # 新しいデータを登録したため、ページを更新後テスト
+        self.assertEqual(1, response.context['questions'].count())
+        self.assertEqual('aaa', response.context['questions'].first().question_text)
