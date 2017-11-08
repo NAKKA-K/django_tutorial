c2b374d5f4063e8e2a681194426f61e2ba97f189
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-11-08 11:09:01 +0900

    公開した投稿だけを取得するクラスメソッドを作成

--- polls/models.py
+++ polls/models.py
@@ -4,6 +4,9 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class QuestionQuerySet(models.query.QuerySet):
+  def is_published(self): # 実装をmodelsですることも可能だが、querysetを拡張したほうが柔軟性のある呼び出しが可能になる
+    return self.filter(published_date__lte = timezone.now())
 
 class Question(models.Model):
   class Meta:
@@ -14,16 +17,22 @@ class Question(models.Model):
   question_text = models.CharField(max_length=200)
   published_date = models.DateTimeField('date published')
 
-  def was_published_recently(self):
+
+  objects = models.Manager.from_queryset(QuestionQuerySet)()
+  @classmethod
+  def get_published_data(cls): # Question.get_published_data()で公開済みのquerysetが取得できる
+    return cls.objects.is_published()
+
+  def was_published_recently(self): # 一日中に公開したものか？
     return timezone.now() >= self.published_date >= timezone.now() - datetime.timedelta(days = 1)
 
-  def what_days_ago(self):
+  def what_days_ago(self): # 何日前に公開したものか?
     days_ago = (timezone.now() - self.published_date) / datetime.timedelta(days=1)
     return str(int(days_ago)) + '日前'
 
   what_days_ago.admin_order_field = 'published_date' # 並び替えの対象カラム
 
-  def __str__(self):
+  def __str__(self): # これがないと常時されるインスタンスの名前が全てQuestion Objectになる
     return self.question_text
 
 
